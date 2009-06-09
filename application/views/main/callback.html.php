6981b9f2b333d61a150b7f30316885ba8256d855
Karl Bode <kpbode@gmail.com>
2009-06-09 15:29:34 +0200

    test

--- application/views/main/callback.html.php
+++ application/views/main/callback.html.php
@@ -1,8 +1,8 @@
 <?php if ($authenticated) { ?>
+	<?php die('the end'); ?>
 	<h3>Hallo <?php echo $user_data->name; ?>(<?php echo $user_data->screen_name; ?>)</h3>
 	<img src="<?php echo $user_data->profile_image_url; ?>" alt="Profilbild" />
 	<a href="/main/start.kml">geWittr in Google Earth starten</a>
 <?php } else { ?>
 	Du konntest leider nicht authentisiert werden...
 <?php } ?>
-<?php die('the end'); ?>
