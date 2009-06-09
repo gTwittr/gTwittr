663e00e101dbed953612334702443d6029b95ffb
Karl Bode <kpbode@gmail.com>
2009-06-09 15:30:54 +0200

    test

--- application/views/main/callback.html.php
+++ application/views/main/callback.html.php
@@ -1,6 +1,5 @@
 <?php if ($authenticated) { ?>
-	<?php die('the end'); ?>
-	<h3>Hallo <?php echo $user_data->name; ?>(<?php echo $user_data->screen_name; ?>)</h3>
+	<h3>Hallo <?php echo $user_data->name; ?> 	<?php die('the end'); ?>(<?php echo $user_data->screen_name; ?>)</h3>
 	<img src="<?php echo $user_data->profile_image_url; ?>" alt="Profilbild" />
 	<a href="/main/start.kml">geWittr in Google Earth starten</a>
 <?php } else { ?>
