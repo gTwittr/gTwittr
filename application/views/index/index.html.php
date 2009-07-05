fe3ca64645198c56e2b5972c39e25afa2d3fbc50
Karl Bode <kpbode@gmail.com>
2009-07-05 17:59:39 +0200

    test library existance

--- application/views/index/index.html.php
+++ application/views/index/index.html.php
@@ -1,3 +1,5 @@
+<img src="<?php echo GraphicService::getInstance()->generateProfileImage('http:\/\/s3.amazonaws.com\/twitter_production\/profile_images\/69546449\/2793452349_794ae79227_mini_studi_normal.jpg','#FF0000'); ?>" alt="bla" />
+
 <?php if($authenticated) { ?>
 	<?php echo link_tag('Geotwitter starten','main/start.kml'); ?>
 <?php } else { ?>
